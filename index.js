d0ec4d9bc4f71d77335b5d319b93be82ea4308fa
PG6402 <tranphat701@gmail.com>
2024-02-02 19:58:37 +0700

    third

--- index.js
+++ index.js
@@ -13,8 +13,6 @@ server.get('/echo', (req, res) => {
   res.jsonp(req.query);
 });
 
-// To handle POST, PUT and PATCH you need to use a body-parser
-// You can use the one used by JSON Server
 server.use(jsonServer.bodyParser);
 server.use((req, res, next) => {
   if (req.method === 'POST') {
